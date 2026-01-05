50b0d0deefb9285d3277b26bb74531b9ab47ce0a
Akshay269 <aksrahangdale@gmail.com>
2026-01-05 14:22:53 +0530

    commiting first record

---
+++ src/main/java/com/example/task_app/domain/entity/TaskPriority.java
@@ -0,0 +1,8 @@
+package com.example.task_app.domain.entity;
+
+public enum TaskPriority {
+    HIGH,
+    MEDIUM,
+    LOW
+
+}
---
+++ src/main/java/com/example/task_app/domain/entity/TaskStatus.java
@@ -0,0 +1,6 @@
+package com.example.task_app.domain.entity;
+
+public enum TaskStatus {
+    OPEN,
+    COMPLETE
+}
